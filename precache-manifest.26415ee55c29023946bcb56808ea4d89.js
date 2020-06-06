d6583c5cadfcfb038639a44006382fe8f5d840ae
Abin Wolfiez <abinthomasct@gmail.com>
2020-06-07 03:49:04 +0530

    Updates

---
+++ precache-manifest.26415ee55c29023946bcb56808ea4d89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5becc933bc47348a39c022e17c61862a",
+    "url": "/trial/index.html"
+  },
+  {
+    "revision": "d0fe9a70d0eaf8a3aa3f",
+    "url": "/trial/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "ad075e68631f83e25fbf",
+    "url": "/trial/static/js/2.2f5ef2c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/trial/static/js/2.2f5ef2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0fe9a70d0eaf8a3aa3f",
+    "url": "/trial/static/js/main.e46646f0.chunk.js"
+  },
+  {
+    "revision": "a09ce3313a834287309a",
+    "url": "/trial/static/js/runtime-main.ef53127e.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/trial/static/media/logo.ee7cd8ed.svg"
+  }
+]);
